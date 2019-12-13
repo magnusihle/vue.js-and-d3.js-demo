2c77a0ea11fe9937d3c43b0158e801d5ded55862
Magnus Ihle <magnus.ihle@sysco.no>
2019-12-13 10:25:18 +0100

    vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,151 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <svg width="500" height="500">
+      <g
+        class="flower"
+        v-for="flower in layoutData.children"
+        :key="flower.data.name"
+        :style="{
+          transform: `translate(${flower.x}px, ${flower.y}px)`
+        }"
+      >
+        <circle
+          class="flower__circle"
+          :r="flower.r"
+          :fill="flower.data.color"
+        />
+        <text class="flower__label">{{ flower.data.name }}</text>
+      </g>
+    </svg>
+    <div class="controls">
+      <v-row>
+        <div class="control" v-for="flower in flowers" :key="flower.name">
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+            ></v-text-field>
+          </v-col>
+          <label>{{ flower.name }}</label>
+          <input type="number" v-model="flower.amount" step="10" min="10" />
+        </div>
+      </v-row>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { hierarchy, pack } from "d3-hierarchy";
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      flowers: [
+        {
+          name: "Roses",
+          amount: 25,
+          color: "#DA627D"
+        },
+        {
+          name: "Tulips",
+          amount: 40,
+          color: "#69B578"
+        },
+        {
+          name: "Daisies",
+          amount: 15,
+          color: "#CCFBFE"
+        },
+        {
+          name: "Narcissuses",
+          amount: 9,
+          color: "#D0DB97"
+        },
+        {
+          name: "Violettes",
+          amount: 21,
+          color: "#D6A2AD"
+        }
+      ]
+    };
+  },
+
+  computed: {
+    transformedFlowerData() {
+      return {
+        name: "Top Level",
+        children: this.flowers.map(flower => ({
+          ...flower,
+          size: flower.amount,
+          parent: "Top Level"
+        }))
+      };
+    },
+
+    layoutData() {
+      // Generate a D3 hierarchy
+      const rootHierarchy = hierarchy(this.transformedFlowerData)
+        .sum(d => d.size)
+        .sort((a, b) => {
+          return b.value - a.value;
+        });
+
+      // Pack the circles inside the viewbox
+      return pack()
+        .size([500, 500])
+        .padding(10)(rootHierarchy);
+    }
   }
 };
 </script>
+
+<style>
+body {
+  font: 16px -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
+    Arial, sans-serif;
+}
+
+svg {
+  display: block;
+  margin: 0 auto;
+}
+
+.flower {
+  transition: transform 0.2s ease-in-out;
+  text-anchor: middle;
+}
+
+.flower__circle {
+  transition: r 0.2s ease-in-out;
+}
+
+.flower__label {
+  fill: #1c2321;
+  font-weight: bold;
+  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.control {
+  display: inline-flex;
+  flex-direction: column;
+  margin: 0 4px;
+}
+
+.control label {
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 4px;
+}
+
+.control input {
+  display: block;
+  font: inherit;
+  width: 100px;
+}
+</style>
